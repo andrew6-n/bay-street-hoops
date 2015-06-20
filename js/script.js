8f562ed509d3c218e1bf30cc7ef3deaa1d997c6d
rex-master <andrewspaceman@gmail.com>
2015-06-20 17:38:11 -0400

    websites

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
     $(".logo").hide().delay(1000).show("400");
+    
 });
-
+$( document ).ready(function() {
+    $(".about-logo").hide().delay(1000).show("400");
+    
+});
